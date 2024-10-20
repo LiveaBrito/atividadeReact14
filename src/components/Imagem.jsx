4196aea9884fd313b973cc41994e65c3341ac81a
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 02:55:48 -0300

    Add files via upload

---
+++ src/components/Imagem.jsx
@@ -0,0 +1,10 @@
+
+function Imagem(){
+    return (
+        <>
+        <img src="https://img.freepik.com/premium-photo/flowers-wallpaper-8k-3d-hyperrealistic_910325-5797.jpg" alt="Uma imagem com flores 3d com uma tonalidade um pouco escura, porém não deixando de ser coloridas, além de um sombreado na imagem" class="img" />
+        </>
+    )
+}
+
+export default Imagem
